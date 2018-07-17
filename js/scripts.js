809cadba52096cd27490ccc31388e912ec69752a
cynthiaokello <cynthiaokello050@gmail.com>
2018-07-17 11:44:55 +0300

    made changes to the content

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 //business logic
 function Contact(first,last) {
 this.firstName = first;
-this.lastName =last;
+this.lastName = last;
+}
+Contact.prototype.fullName = function() {
+  return this.firstName + "" + this.lastName;
 }
 
 // user interface logic
@@ -14,7 +17,7 @@ this.lastName =last;
 
       var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName () + "</span></li>");
 
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
