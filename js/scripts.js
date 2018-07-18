ba68643675ddd218d21c104c54cac839eb42b7e5
cynthiaokello <cynthiaokello050@gmail.com>
2018-07-18 12:31:22 +0300

    added some content to the file

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,12 @@ this.lastName = last;
 Contact.prototype.fullName = function() {
   return this.firstName + "" + this.lastName;
 }
+function Address(street, city, county) {
+  this.street = street;
+  this.city = city;
+  this.county = county;
+}
+
 
 // user interface logic
   $(document).ready(function() {
@@ -18,7 +24,7 @@ Contact.prototype.fullName = function() {
       var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
       $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName () + "</span></li>");
-      
+
 
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
@@ -31,3 +37,31 @@ Contact.prototype.fullName = function() {
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
   });
+
+  $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-county">County</label>' +
+                                   '<input type="text" class="form-control new-county">' +
+                                 '</div>' +
+                               '</div>');
+  });
+...
+...
+$(".new-address").each(function() {
+  var inputtedStreet = $(this).find("input.new-street").val();
+  var inputtedCity = $(this).find("input.new-city").val();
+  var inputtedCounty = $(this).find("input.new-county").val();
+  var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+  newContact.addresses.push(newAddress);
+});
+...
